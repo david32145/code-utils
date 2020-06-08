d8009f57052ec819499c1dd80a0b206efa84e019
david32145 <nascimento32145@gmail.com>
2020-06-08 19:56:06 -0300

    add typescript example

---
+++ observable/observable.spec.ts
@@ -0,0 +1,33 @@
+import Subject from "./observable"
+
+interface Message {
+  sender: string // Remetente da messagem
+  content: string // Conteúdo da messagem
+}
+
+const chat = new Subject<Message>()
+
+const johnId = chat.subscribe((message) => {
+  if(message.sender === "john") return
+  console.log(`> John receive message ${message.content} from ${message.sender}`)
+})
+
+const maryId = chat.subscribe((message) => {
+  if(message.sender === "mary") return
+  console.log(`> Mary receive message ${message.content} from ${message.sender}`)
+})
+
+const julyId = chat.subscribe((message) => {
+  if(message.sender === "july") return
+  console.log(`> July receive message ${message.content} from ${message.sender}`)
+})
+
+chat.publish({
+  sender: "john",
+  content: "Hello guys"
+})
+
+chat.publish({
+  sender: "july",
+  content: "What are they doing?"
+})
